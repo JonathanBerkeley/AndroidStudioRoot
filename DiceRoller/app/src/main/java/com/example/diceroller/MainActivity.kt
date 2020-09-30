4bbe81164cca46cccc7b613d2220e5c092b5e319
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2020-09-30 03:39:17 +0100

    Finished additional dice functionality

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -9,13 +9,19 @@ import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var diceImage: ImageView
-    lateinit var rT : TextView
+    lateinit var diceImageA: ImageView
+    lateinit var diceImageB: ImageView
+    lateinit var resultTextA : TextView
+    lateinit var resultTextB : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        diceImage = findViewById(R.id.dice_image)
-        rT = findViewById(R.id.result_text)
+        diceImageA = findViewById(R.id.dice_imageA)
+        diceImageB = findViewById(R.id.dice_imageB)
+        resultTextA = findViewById(R.id.result_textA)
+        resultTextB = findViewById(R.id.result_textB)
+
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
             rollDice()
@@ -35,8 +41,43 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
         var randomInt: Int = (1..6).random()
-        rT.text = randomInt.toString()
-        val drawableRes = when (randomInt) {
+        var randomInt2: Int = (1..6).random()
+        resultTextA.text = randomInt.toString()
+        resultTextB.text = randomInt2.toString()
+        diceImageA.setImageResource(manageDiceRes(randomInt))
+        diceImageB.setImageResource(manageDiceRes(randomInt2))
+
+    }
+    private fun countUp() {
+        when {
+            //Edge case requirements
+            resultTextA.text.toString() == getString(R.string.hello_world) -> resultTextA.text = "1"
+            resultTextA.text.toString() == "6" -> {}
+            else -> {
+                var i : Int = resultTextA.text.toString().toInt()
+                resultTextA.text = (++i).toString()
+            }
+        }
+        when {
+            resultTextB.text.toString() == "" -> resultTextB.text = "1"
+            resultTextB.text.toString() == "6" -> {}
+            else -> {
+                var i : Int = resultTextB.text.toString().toInt()
+                resultTextB.text = (++i).toString()
+            }
+        }
+        //Redraw the dice image to new number
+        diceImageA.setImageResource(manageDiceRes(resultTextA.text.toString().toInt()))
+        diceImageB.setImageResource(manageDiceRes(resultTextB.text.toString().toInt()))
+    }
+    private fun resetCount() {
+        resultTextA.text = "0"
+        resultTextB.text = "0"
+        diceImageA.setImageResource(R.drawable.empty_dice)
+        diceImageB.setImageResource(R.drawable.empty_dice)
+    }
+    private fun manageDiceRes(decider : Int) : Int {
+        return when (decider) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -44,33 +85,5 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource(drawableRes)
-    }
-    private fun countUp() {
-        when {
-            //Edge case requirements
-            rT.text.toString() == getString(R.string.hello_world) -> rT.text = "1"
-            rT.text.toString() == "6" -> {}
-            else -> {
-                var i : Int = rT.text.toString().toInt()
-                rT.text = (++i).toString()
-            }
-        }
-        //Redraw the dice image
-        val drawableRes = when (rT.text.toString().toInt()) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> {
-                R.drawable.dice_6
-            }
-        }
-        diceImage.setImageResource(drawableRes)
-    }
-    private fun resetCount() {
-        rT.text = "0"
-        diceImage.setImageResource(R.drawable.empty_dice)
     }
 }
